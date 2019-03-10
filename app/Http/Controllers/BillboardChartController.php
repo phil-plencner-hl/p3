40b36b4b521b3f10a2a78dfd320ed79a577cc527
Phil Plencner <philip_plencner@harvard.edu>
2019-03-10 17:54:23 -0400

    Added Billboard Chart Controller and Routes

---
+++ app/Http/Controllers/BillboardChartController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BillboardChartController extends Controller
+{
+    public function index()
+    {
+        return 'Display the Billboard chart form.';
+    }
+
+    public function showChart()
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BillboardChartController@index');
+Route::get('/show-chart', 'BillboardChartController@showChart');
